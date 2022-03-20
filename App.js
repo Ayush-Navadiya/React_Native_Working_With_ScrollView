120ced06a39f66455cb2d0aa42c4e1a33c507c04
Ayush-Navadiya <ayushnavadiya@gmail.com>
2022-03-20 22:36:58 +0530

    initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      {/* Upper View */}
+      <View style={styles.container1}>
+        <Text>Stuff in view 1!</Text>
+      </View>
+      {/* Lower View */}
+      <View style={styles.container2}>
+        <Text style={{color: '#fff'}}>Stuff in view 2!</Text>
+      </View>
+      <View style={styles.container1}>
+        <Text>Stuff in view 3!</Text>
+      </View>
+      <View style={styles.container2}>
+        <Text style={{color: '#fff'}}>Stuff in view 4!</Text>
+      </View>
+    </ScrollView>
   );
 }
 
+// container Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    },
+  container1: {
+    flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
+    width: '100%',
+    height: 300,
+    justifyContent: 'center',
+  },
+  container2: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    width: '100%',
+    height: 300,
     justifyContent: 'center',
   },
 });
+
